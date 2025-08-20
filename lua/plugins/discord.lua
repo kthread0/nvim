328d54f0183f43b00d3d317cd7da96c93a173fdc
kthread0 <dev@kthread.dev>
2025-08-20 12:49:23 -0300

    yay more discord

--- lua/plugins/discord.lua
+++ lua/plugins/discord.lua
@@ -1,1 +1,34 @@
-return { { "vyfor/cord.nvim", build = ":Cord update", opts = {} } }
+return {
+	"vyfor/cord.nvim",
+	build = ":Cord update",
+	config = function()
+		require("cord").setup({
+			display = {
+				swap_fields = true,
+			},
+			timestamp = {
+				enabled = true,
+				shared = true,
+			},
+			advanced = {
+				plugin = { cursor_update = "on_move" },
+				server = {
+					update = "build",
+				},
+				discord = {
+					reconnect = {
+						enabled = true,
+					},
+				},
+			},
+			plugins = {
+				"cord.plugins.diagnostics", -- Enable diagnostics plugin with default settings
+
+				["cord.plugins.diagnostics"] = { -- Enable AND configure diagnostics plugin
+					scope = "workspace", -- Set scope to 'workspace' instead of default 'buffer'
+					severity = vim.diagnostic.severity.WARN, -- Show warnings and above
+				},
+			},
+		})
+	end,
+}
