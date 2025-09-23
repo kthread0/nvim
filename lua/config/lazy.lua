9eff0b791d0109052e81324704a36179566f54d5
kthread0 <dev@kthread.dev>
2025-09-23 12:10:44 -0300

    fix, remove neovide

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -53,11 +53,11 @@ require("lazy").setup({
 		-- set to `nil` to use hererocks when luarocks is not found
 		-- set to `true` to always use hererocks
 		-- set to `false` to always use luarocks
-		hererocks = false,
+		hererocks = nil,
 	},
 	-- Configure any other settings here. See the documentation for more details.
 	-- colorscheme that will be used when installing plugins.
-	install = { colorscheme = { "carbonfox" } },
+	install = { colorscheme = { "kanawaga" } },
 	-- automatically check for plugin updates
 	checker = { enabled = true },
 	ui = {
